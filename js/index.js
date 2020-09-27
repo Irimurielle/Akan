4756b7e110143ecd9dfb53bf229c0597579697fc
Irimurielle <irimurielle@gmail.com>
2020-09-27 13:41:00 +0200

    arrays of names and day week formula

--- js/index.js
+++ js/index.js
@@ -15,3 +15,6 @@ var day = function() {
         return alert("Enter a valid day");
     }
 }
+var dayOfWeek = Math.floor(((((YY.slice(0, 2)) / 4) - 2 * (YY.slice(0, 2)) - 1) + ((5 * (YY.slice(2, 4)) / 4)) + ((26 * (MM + 1) / 10)) + DD) % 7);
+var maleAkanNames = ["Kwasi", "Kwadwo", "Kwabena", "Kwaku", "Kwaku", "Yaw", "Kofi", "Kwame"];
+var femaleAkanNames = ["Akosua", "Adwoa", "Abenaa", "Akua", "Yaa", "Afua", "Ama"];
